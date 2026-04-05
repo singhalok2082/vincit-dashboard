58d1a54ee3ce4f8c7da7a441d3118cc7e8d5cd88
ConsultAdd <consultadd@ConsultAdds-MacBook-Pro.local>
2026-04-05 20:31:17 +0530

    fix: delete App.js, keep App.jsx, vite handles .js as jsx

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    include: '**/*.{jsx,js}',
+  })],
   build: {
     outDir: 'build'
-  }
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
